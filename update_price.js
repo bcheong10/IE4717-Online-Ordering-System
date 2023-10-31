03470c78cd99affa392a0b4761e2ea5494c95713
izack8 <isaactaypb@gmail.com>
2023-10-31 19:34:22 +0800

    ui updates session created for cart

--- update_price.js
+++ update_price.js
@@ -1,9 +1,15 @@
 function updatePriceLabel() {
     'use strict';
-    var pasta_price = document.getElementById('pasta_drop').value;
-    var base_price = document.getElementById('base_drop').value;
-    var meat_price = document.getElementById('meat_drop').value;
-    var veg_price = document.getElementById('veg_drop').value;
+    var pasta_string = document.getElementById('pasta_drop').value;
+    var base_string = document.getElementById('base_drop').value;
+    var meat_string = document.getElementById('meat_drop').value;
+    var veg_string = document.getElementById('veg_drop').value;
+
+    var pasta_price = pasta_string.split(",")[1];
+    var base_price = base_string.split(",")[1];
+    var meat_price = meat_string.split(",")[1];
+    var veg_price = veg_string.split(",")[1];
+
 
     document.getElementById('price').innerText = "$" + String(parseFloat(pasta_price) + parseFloat(base_price) + 
                                                 parseFloat(meat_price) + parseFloat(veg_price));
