1db9ca459a15bd7286adcffd7aceb104c8f7d409
izack8 <isaactaypb@gmail.com>
2023-11-02 20:17:02 +0800

    merged with ben + minor changes

--- checkout.php
+++ checkout.php
@@ -7,234 +7,308 @@ var_dump($_SESSION);
 ?>
 <!DOCTYPE html>
 <html lang="en">
+    <script src='functions.js'></script>
+
 <head>
-<title>Pasta!</title>
-<meta charset="utf-8">
-<style>
-a{
-    text-decoration: none;
-    color: white;
-}
-nav {
-    background-color: rgba(0, 0, 0, 0.7); /* Translucent black background */
-    overflow: hidden;
-}
+    <title>Pasta!</title>
+    <meta charset="utf-8">
+    <style>
+        a {
+            text-decoration: none;
+            color: white;
+        }
 
-ul.navbar {
-    list-style-type: none;
-    padding: 0;
-    margin: 0;
-    text-align: center;
-    font-family:'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif
-}
+        nav {
+            background-color: rgba(0, 0, 0, 0.7);
+            /* Translucent black background */
+            overflow: hidden;
+        }
 
-ul.navbar li {
-    display: inline;
-    padding: 0px 30px 0px 30px;
-}
+        ul.navbar {
+            list-style-type: none;
+            padding: 0;
+            margin: 0;
+            text-align: center;
+            font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif
+        }
 
-ul.navbar li a {
-    text-decoration: none;
-    color: white;
-    padding: 10px 20px; 
-    display: inline-block;
-}
+        ul.navbar li {
+            display: inline;
+            padding: 0px 30px 0px 30px;
+        }
 
-ul.navbar li a:hover {
-    background-color: rgba(255, 255, 255, 0.2); 
-    color: black;
-}
-#button_container{
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-}
+        ul.navbar li a {
+            text-decoration: none;
+            color: white;
+            padding: 10px 20px;
+            display: inline-block;
+        }
 
-#button_container #buttons{
-    background-color: rgba(0, 0, 0, 0.7); /* Translucent black background */
-    display: block;
-    color: white;
-    padding: 10px 20px; 
-    border-radius: 10px;
-    width: 160px; /* Set the desired width */
-    height: 50px; /* Set the desired height */
-    font-size: 17px;
-    border: none;
-    font-family:'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+        ul.navbar li a:hover {
+            background-color: rgba(255, 255, 255, 0.2);
+            color: black;
+        }
 
-}
+        #button_container {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+        }
 
-#wrapper{
-    margin: 35px 20px 20px 20px;
-    min-width: 90%;
-    height: 500px;
-    border: none;
-    border-radius: 10px;
-    border-color: black;
-    position: relative;
-    display: flex;
-    flex-direction: row;
-}
+        #button_container #buttons {
+            background-color: rgba(0, 0, 0, 0.7);
+            /* Translucent black background */
+            display: block;
+            color: white;
+            padding: 10px 20px;
+            border-radius: 10px;
+            width: 160px;
+            /* Set the desired width */
+            height: 50px;
+            /* Set the desired height */
+            font-size: 17px;
+            border: none;
+            font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
 
-#menu_label{
-    position: absolute;
-    font-size: 25px;
-    padding-left: 5px;
-    padding-right: 5px;
-    background-color: white;
-    margin-left: 18px; 
-    z-index: 1;
-    font-weight: bold;
-    top: -42px;
-}
+        }
 
-#customer_info_label{
-    position: absolute;
-    font-size: 25px;
-    padding-left: 5px;
-    padding-right: 5px;
-    background-color: white;
-    margin-left: 18px; 
-    z-index: 1;
-    font-weight: bold;
-    top: -42px;
-}
+        #wrapper {
+            margin: 35px 20px 20px 20px;
+            min-width: 90%;
+            height: 500px;
+            border: none;
+            border-radius: 10px;
+            border-color: black;
+            position: relative;
+            display: flex;
+            flex-direction: row;
+        }
 
-#order_summary_label{
-    position: absolute;
-    font-size: 25px;
-    padding-left: 5px;
-    padding-right: 5px;
-    background-color: white;
-    margin-left: 18px; 
-    z-index: 1;
-    font-weight: bold;
-    top: -42px;
-}
+        #menu_label {
+            position: absolute;
+            font-size: 25px;
+            padding-left: 5px;
+            padding-right: 5px;
+            background-color: white;
+            margin-left: 18px;
+            z-index: 1;
+            font-weight: bold;
+            top: -42px;
+        }
 
-#left_column{
-    float: left;
-    display: flex;
-    flex-direction: column;
-    width: 48%;
-    border: solid 2px #333;
-    border-radius: 10px;
-    border-color: black;
+        #customer_info_label {
+            position: absolute;
+            font-size: 25px;
+            padding-left: 5px;
+            padding-right: 5px;
+            background-color: white;
+            margin-left: 18px;
+            z-index: 1;
+            font-weight: bold;
+            top: -42px;
+        }
 
-    position: relative;
-}
+        #order_summary_label {
+            position: absolute;
+            font-size: 25px;
+            padding-left: 5px;
+            padding-right: 5px;
+            background-color: white;
+            margin-left: 18px;
+            z-index: 1;
+            font-weight: bold;
+            top: -42px;
+        }
 
-#right_column{
-    float: right;
-    display: flex;
-    flex-direction: column;
-    margin-left: 45px;
-    width: 48%;
-    border: solid 2px #333;
-    border-radius: 10px;
-    border-color: black;
-    
-    position: relative;
-}
+        #left_column {
+            float: left;
+            display: flex;
+            flex-direction: column;
+            width: 48%;
+            border: solid 2px #333;
+            border-radius: 10px;
+            border-color: black;
 
-.details_tb{
-    display: block;
-    margin: 10px 0 0 20px;
-    border: solid 1px #333;
-    border-radius: 10px;
-    background-color: #f1f1f1;
-    font-size: 16px;
-    padding-left: 6px;
-    width: 80%; 
-    height: 30px; 
-    font-family:'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
-}
+            position: relative;
+        }
 
-.secret_details_tb{
-    display: block;
-    margin: 10px 0 0 20px;
-    border: solid 1px #333;
-    border-radius: 10px;
-    background-color: #f1f1f1;
-    font-size: 16px;
-    padding-left: 6px;
-    width: 30%; 
-    height: 30px; 
-    font-family:'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
-}
+        #right_column {
+            float: right;
+            display: flex;
+            flex-direction: column;
+            margin-left: 45px;
+            width: 48%;
+            border: solid 2px #333;
+            border-radius: 10px;
+            border-color: black;
 
-#items_summary{
-    display: flex; 
-    flex-direction: column;
-    margin-top: 10px;
-    font-family:'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
-}
+            position: relative;
+        }
+
+        .details_tb {
+            display: block;
+            margin: 10px 0 0 20px;
+            border: solid 1px #333;
+            border-radius: 10px;
+            background-color: #f1f1f1;
+            font-size: 16px;
+            padding-left: 6px;
+            width: 80%;
+            height: 30px;
+            font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+        }
+
+        .secret_details_tb {
+            display: block;
+            margin: 10px 0 0 20px;
+            border: solid 1px #333;
+            border-radius: 10px;
+            background-color: #f1f1f1;
+            font-size: 16px;
+            padding-left: 6px;
+            width: 30%;
+            height: 30px;
+            font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+        }
+
+        #items_summary {
+            display: flex;
+            flex-direction: column;
+            margin-top: 10px;
+            font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+        }
 
 
-#order_details{
-    font-size: 1.1rem;
-    
-}
+        #order_details {
+            font-size: 1.1rem;
 
-</style> 
+        }
+
+        #subtotal {
+            display: flex;
+            flex-direction: column;
+            position: relative;
+            margin-left: 20px;
+            margin-top: 90px;
+
+        }
+
+        #price_label {
+            position: absolute;
+            font-size: 20px;
+            background-color: white;
+            margin-left: 18px;
+            z-index: 1;
+            font-weight: bold;
+            padding-left: 5px;
+            padding-right: 5px;
+        }
+
+        #price {
+            position: absolute;
+            font-size: 20px;
+            width: 30px;
+            background-color: white;
+            border: solid 1px #333;
+            padding: 24px;
+            padding-right: 88px;
+            margin-top: 30px;
+            z-index: 0;
+            border-radius: 10px;
+            font-weight: bold;
+        }
+
+        #button_container {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+        }
+
+        #button_container #buttons {
+            background-color: rgba(0, 0, 0, 0.7);
+            /* Translucent black background */
+            display: block;
+            color: white;
+            margin-top: 50px;
+            padding: 10px 20px;
+            border-radius: 10px;
+            width: 160px;
+            /* Set the desired width */
+            height: 50px;
+            /* Set the desired height */
+            font-size: 17px;
+            border: none;
+            font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+
+        }
+    </style>
 
 </head>
 
 <body>
-  <header>
-  <nav>
-    <ul class='navbar'>
-      <li><a href="index.php">Home</a></li>
-      <li><a href="order.php">Order Now</a></li>
-      <li><a href="track.html">Track Order</a></li>
-      <li><a href="checkout.php">Checkout</a></li>
-	</ul>
-  </nav>
-  </header>
+    <header>
+        <nav>
+            <ul class='navbar'>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="order.php">Order Now</a></li>
+                <li><a href="track.html">Track Order</a></li>
+                <li><a href="checkout.php">Checkout</a></li>
+            </ul>
+        </nav>
+    </header>
 
-  <div id="wrapper">
-    <div id="left_column">
-    <p id="customer_info_label">Customer Info</p>
+    <div id="wrapper">
+        <div id="left_column">
+            <p id="customer_info_label">Customer Info</p>
 
-    <input type="text" class="details_tb" style="margin-top: 29px" placeholder="Cardholder's Name">
-    <input type="text" class="details_tb" placeholder="Card Number">
+            <input type="text" class="details_tb" style="margin-top: 29px" placeholder="Cardholder's Name">
+            <input type="text" class="details_tb" placeholder="Card Number">
 
-    <input type="month" class="secret_details_tb" style="width: 30%; margin-top: 29px" placeholder="MM/YY"> 
-    <!--maybe use regex?-->
-    <input type="text" class="secret_details_tb" style="width: 30%; margin-top: 29px" placeholder="CVV">
-    <input type="email" class="details_tb" style="margin-top: 29px" placeholder="Email">
+            <input type="month" class="secret_details_tb" style="width: 30%; margin-top: 29px" placeholder="MM/YY">
+            <!--maybe use regex?-->
+            <input type="text" class="secret_details_tb" style="width: 30%; margin-top: 29px" placeholder="CVV">
+            <input type="email" class="details_tb" style="margin-top: 29px" placeholder="Email">
 
-    </div>
+            <div id="subtotal">
+                <p id="price_label">Subtotal</p>
+                <p id="price">$0.00</p>
+            </div>
 
-    <div id="right_column">
-    <p id="order_summary_label">Order Summary</p>
+            <div id="button_container">
+                    <button id="buttons" onclick="clearCart();">Clear Order</button><br>  
+            </div>
 
-    <div id="items_summary">
-        <ul>
-            <?php 
-                if(count($cart) > 0){
-                    for ($i = 0; $i < count($cart); $i++) {
-                        echo "<li style='font-size: 25px; margin-bottom: 15px;'> Order ".($i+1).": $".$cart[$i][5]."<ul id='order_details'>"
-                        ."<li>".$cart[$i][0] ."</li>".
-                        "<li>".$cart[$i][1]."</li>".
-                        "<li>".$cart[$i][2]."</li>".
-                        "<li>".$cart[$i][3]."</li>".
-                        "</ul>".
-                        "</li>";  
+        </div>
+
+        <div id="right_column">
+            <p id="order_summary_label">Order Summary</p>
+
+            <div id="items_summary">
+                <ul>
+                    <?php
+                    if (count($cart) > 0) {
+                        for ($i = 0; $i < count($cart); $i++) {
+                            echo "<li style='font-size: 25px; margin-bottom: 15px;'> Order " . ($i + 1) . ": $" . $cart[$i][5] . "<ul id='order_details'>"
+                                . "<li>" . $cart[$i][0] . "</li>" .
+                                "<li>" . $cart[$i][1] . "</li>" .
+                                "<li>" . $cart[$i][2] . "</li>" .
+                                "<li>" . $cart[$i][3] . "</li>" .
+                                "</ul>" .
+                                "</li>";
+                        }
+                    } else {
+                        echo "Nothing in cart!";
                     }
-                }else{
-                    echo "Nothing in cart!";
-                }
-            ?>
-        </ul>
+                    ?>
+                </ul>
+            </div>
+
+
+
+        </div>
+
+
     </div>
-
-
-
-    </div>
-
-
-  </div>
 
 </body>
 
@@ -243,5 +317,3 @@ ul.navbar li a:hover {
 </footer>
 
 </html>
-
-
--- functions.js
+++ functions.js
@@ -8,3 +8,21 @@ function updatePriceLabel() {
     document.getElementById('price').innerText = "$" + String(parseFloat(pasta_price) + parseFloat(base_price) + 
                                                 parseFloat(meat_price) + parseFloat(veg_price));
 }
+
+function clearCart(){
+        fetch("clear_cart.php", {
+          method: "POST", // You can use POST or GET depending on your needs
+        })
+          .then(function (response) {
+            if (response.status === 200) {
+              // Handle the success, e.g., provide a confirmation message
+              console.log("Array cleared successfully.");
+            } else {
+              // Handle errors, e.g., display an error message
+              console.error("Failed to clear the array.");
+            }
+          })
+          .catch(function (error) {
+            console.error("Error:", error);
+          });
+    };
